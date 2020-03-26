43d7eecbcec5cd1105bdcf6126483f6a1d74b7bc
Scott Key <scottdkey@gmail.com>
2020-03-25 19:52:26 -0600

    Fetch User works

--- client/src/components/FetchUser.js
+++ client/src/components/FetchUser.js
@@ -1,49 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-import { AuthConsumer, } from "../providers/AuthProvider";
+import { AuthContext, } from "../providers/AuthProvider";
 
-class FetchUser extends React.Component {
-  state = { loaded: false, };
+const FetchUser =(props)=> {
+  const auth = useContext(AuthContext)
+  const [loaded, setLoaded] = useState(false)
 
-  componentDidMount() {
-    const { auth: { authenticated, setUser, }, } = this.props;
-
-    if (authenticated) {
-      this.loaded();
-    } else {
-      if (this.checkLocalToken()) {
-        axios.get('/api/auth/validate_token')
-          .then( res => {
-            setUser(res.data.data);
-            this.loaded();
-          })
-          .catch( res => {
-            this.loaded();
-          })
-      } else {
-        this.loaded();
-      }
-  }
-}
-
-checkLocalToken = () => {
+const checkLocalToken = () => {
   const token = localStorage.getItem('access-token');
   return token;
 }
 
-loaded = () => this.setState({ loaded: true, });
-
-render() {
-  return this.state.loaded ? this.props.children : null;
-}
-}
-
-const ConnectedFetchUser = (props) => (
-<AuthConsumer>
-  { auth => 
-    <FetchUser { ...props } auth={auth} />
+useEffect( () => {
+  const authenticated = auth.authenticated
+  if(authenticated){
+    setLoaded(true)
+  } else {
+    if (checkLocalToken()){
+      axios.get('api/auth/validate_token')
+      .then( res => {
+        props.setUser(res.data.data)
+        setLoaded(true)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
   }
-</AuthConsumer>
-)
+  setLoaded(true)
+})
 
-export default ConnectedFetchUser;
+  return (
+    <>
+    {loaded ? props.children : null}
+    </>
+  ) 
+
+}
+
+
+export default FetchUser;
--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -1,82 +1,94 @@
-import React from 'react'
-import axios from 'axios'
-import {Form} from 'semantic-ui-react'
-import useFormInput from '../hooks/useFormInput'
+import React from "react";
+import axios from "axios";
+import { Form } from "semantic-ui-react";
+import { AuthConsumer } from "../providers/AuthProvider";
+import { Redirect } from "react-router-dom";
+class ProfileForm extends React.Component {
+  state = {
+    name: this.props.auth.user.name,
+    nickname: this.props.auth.user.nickname,
+    email: this.props.auth.user.email,
+    image: this.props.auth.user.image,
+    redirect: null
+  };
 
-const ProfileForm =(props) => {
-  const name = useFormInput('')
-  const nickname = useFormInput('')
-  const email = useFormInput('')
-  const image = useFormInput('')
+  handleChange = e => {
+    const {
+      target: { name, value }
+    } = e;
+    this.setState({
+      [name]: value
+    });
+  };
 
-  const clearState = () => {
-    name.clear()
-    nickname.clear()
-    email.clear()
-    image.clear()
-  }
-
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    console.log(props)
-    const userID = props.auth.user.id
+  handleSubmit = e => {
+    console.log(this.props);
+    const userID = this.props.auth.user.id;
+    const user = { ...this.state };
+    console.log(user);
     axios
-      .put(`/api/users/${userID}`, { 
-        name: name.value, 
-        nickname: nickname.value,
-        email: email.value,
-        image: image.value
-      
-      })
+      .put(`/api/users/${userID}`, user)
       .then(res => {
-        props.add(res.data)
-        clearState();
-        props.toggleForm(false)
+        this.setState({ redirect: "/profile" });
       })
       .catch(err => {
         console.log(err.response);
       });
   };
 
+  render() {
+    const { name, nickname, image, email } = this.state;
+
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
+    }
     return (
       <div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group width='equal'>
-          <Form.Input
-            label="Name"
-            placeholder="Fill in Name"
-            name="name"
-            {...name}
-            />
-          <Form.Input
-            label="Nickname"
-            placeholder="Fill in Nickname"
-            name="nickname"
-            {...nickname}
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group width="equal">
+            <Form.Input
+              label="Name"
+              placeholder="Fill in Name"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
             />
             <Form.Input
-            label="Image Url"
-            placeholder="Fill in Url"
-            name="image"
-            {...image}
+              label="Nickname"
+              placeholder="Fill in Nickname"
+              name="nickname"
+              value={nickname}
+              onChange={this.handleChange}
             />
             <Form.Input
-            label="Email"
-            placeholder="Email"
-            name="email"
-            {...email}
+              label="Image Url"
+              placeholder="Fill in Url"
+              name="image"
+              value={image}
+              onChange={this.handleChange}
             />
-        </Form.Group>
+            <Form.Input
+              label="Email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
 
-        
-        <Form.Button>Submit</Form.Button>
-      </Form>
+          <Form.Button>Submit</Form.Button>
+        </Form>
       </div>
-    
-    )
+    );
   }
+}
 
-
-
-export default ProfileForm
+export default class ConnectedProfileForm extends React.Component {
+  render() {
+    return (
+      <AuthConsumer>
+        {auth => <ProfileForm {...this.props} auth={auth} />}
+      </AuthConsumer>
+    );
+  }
+}
